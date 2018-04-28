a1f2c61b6ac113ee63d69606ba12dd2ef6a4350b
Billy2536292424 <2536292424@qq.com>
2018-04-28 17:36:39 +0800

    Create 04.c

---
+++ 04.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/wait.h>
+#include<errno.h>
+int mysystem(const char* cmd)
+{
+pid_t pid;
+int status;
+if(cmd ==NULL) return 1;
+if((pid = fork()) == -1) return -1;
+if(pid ==0) {//child
+execl("/bin/sh","sh","-c",cmd,NULL);
+exit(127);
+}
+else
+{//parent
+while(waitpid(pid,&status, 0 )==-1)
+{
+
+	if(errno == EINTR) continue;
+	status = -1;
+	break;
+
+}
+}
+}
+int main()
+{
+	mysystem("ls -l | wc -l");
+}
